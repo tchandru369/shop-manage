758a367d847a3c890cba361bf478a0d2ed28b28c
Chandru T <101269738+tchandru369@users.noreply.github.com>
2026-07-23 22:04:03 +0530

    repo issue resolved

--- src/main/java/com/merchant/management/repository/CustAddRewardDetailRepo.java
+++ src/main/java/com/merchant/management/repository/CustAddRewardDetailRepo.java
@@ -2,6 +2,8 @@ package com.merchant.management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustAddRewardDetailRepo extends JpaRepository<CustAddRewardDetailRepo, Integer>{
+import com.merchant.management.entity.CustAddRewardDetails;
+
+public interface CustAddRewardDetailRepo extends JpaRepository<CustAddRewardDetails, Integer>{
 
 }
